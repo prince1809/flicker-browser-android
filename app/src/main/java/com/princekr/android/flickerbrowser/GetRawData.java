e0c8814281646b5db9e42b49a393d3ea459c0f2d
prince <prince18pat@gmail.com>
2019-06-30 02:43:34 +0900

    add fetch data class

---
+++ app/src/main/java/com/princekr/android/flickerbrowser/GetRawData.java
@@ -0,0 +1,61 @@
+package com.princekr.android.flickerbrowser;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALISED, FAILED_OR_EMPTY, OK}
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+
+    public GetRawData() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        if(strings == null) {
+            mDownloadStatus = DownloadStatus.NOT_INITIALISED;
+            return null;
+        }
+
+        try {
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: The response code was " + response);
+
+            StringBuilder result = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "doInBackground: Invalid URL " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "doInBackground: IO exception reading data" + e.getMessage());
+        } catch (SecurityException e) {
+            Log.e(TAG, "doInBackground: Security Exception. Needs permission?" + e.getMessage());
+        }
+        return null;
+    }
+}
